138905eec008639bc2af651c01de35d3b3e97d88
trilleplay <trille@trilleplay.net>
2019-01-28 18:10:06 +0100

    duplicate line

--- setup.py
+++ setup.py
@@ -40,6 +40,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 2 :: Only",
-        "License :: OSI Approved :: MIT License",
     ],
 )
